8e7b592fe108aee537b2d766c02b73b24c3ef250
tyler <tyler@TYLER-T530>
2023-12-01 11:59:55 -0600

    Add project files.

---
+++ advent2023/advent2023.c
@@ -0,0 +1,12 @@
+﻿// advent2023.cpp : Defines the entry point for the application.
+//
+
+#include "advent2023.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ advent2023/advent2023.h
@@ -0,0 +1,9 @@
+﻿// advent2023.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+
+// TODO: Reference additional headers your program requires here.
